5100c0a39df6b02775b675e5a4d05aba40ecb1f6
Hussien Naser <hseennaser111@gmail.com>
2022-11-14 11:40:03 +0200

    adding error message when the access to location is denied

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import React from "react";
 class App extends React.Component{
   constructor (props){
     super(props)
-
-    this.state={lat:400}
+    // THIS THIS THE ONLY TIME we do direct assigment to 
+    // thes.state 
+    this.state={lat:null , err:""}
 
     window.navigator.geolocation.getCurrentPosition(
-      position => {this.setState({lat:position.coords.latitude})},
-      err => console.log(err)
+      // we called set.State !!!!!!!!!!!!!!!!!
+      position => this.setState({lat:position.coords.latitude}),
+      err => this.setState({err:err.message})
     )
   }
 
   render(){
    
-    return <div>Latitude: {this.state.lat} </div>
+    return <div>Latitude: {this.state.lat} 
+            <br /> 
+            Error:{this.state.err}
+
+    </div>
+    
+    
   }
   
 }
